c524fd57dc21a414c87109f420bdde8b91fa6c96
XZEERH <razzyr408@gmail.com>
2026-04-27 17:26:48 +0700

    Create Typewriter.tsx

---
+++ src/components/Typewriter.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const words = ["Welcome To Website TH", "Website Utama Media Global"];
+
+export default function Typewriter() {
+  const [index, setIndex] = useState(0);
+  const[subIndex, setSubIndex] = useState(0);
+  const [reverse, setReverse] = useState(false);
+
+  useEffect(() => {
+    if (subIndex === words[index].length + 1 && !reverse) {
+      setTimeout(() => setReverse(true), 1500);
+      return;
+    }
+    if (subIndex === 0 && reverse) {
+      setReverse(false);
+      setIndex((prev) => (prev + 1) % words.length);
+      return;
+    }
+    const timeout = setTimeout(() => {
+      setSubIndex((prev) => prev + (reverse ? -1 : 1));
+    }, Math.max(reverse ? 50 : 100, Math.random() * 150));
+
+    return () => clearTimeout(timeout);
+  },[subIndex, index, reverse]);
+
+  return <span className="text-neonBlue tracking-wider">{`${words[index].substring(0, subIndex)}|`}</span>;
+}
