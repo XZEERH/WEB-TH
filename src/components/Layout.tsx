d5361bec0cb82fb054019216b3ec6a0ca21bd1d5
XZEERH <razzyr408@gmail.com>
2026-04-27 17:16:40 +0700

    Create Layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,7 @@
+const pages =[
+  { path: '/', name: 'Home', isHtml: false },
+  { path: '/media', name: 'Media', isHtml: false },
+  { path: '/struktur', name: 'Struktur Admin', isHtml: false },
+  { path: '/pages/admin.html', name: 'Halaman Admin', isHtml: true }, // Ini nembak ke file HTML murni
+  { path: '/tentang', name: 'Tentang Kami', isHtml: false }
+];
